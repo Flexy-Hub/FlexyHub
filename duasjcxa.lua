99ab04ddcd8796cf19436e70572a8aa70ded6da4
Flexy-Hub <unknownflexyz@gmail.com>
2025-04-01 02:43:27 +0700

    Create duasjcxa.lua

---
+++ duasjcxa.lua
@@ -0,0 +1,18 @@
+_G.Settings.Mob_Selection_Model = "JB1"
+
+function AreAllMobsDead()
+    for i2,v2 in pairs(workspace.__Main.__Enemies.Client:GetChildren()) do
+        for i,v in v2:GetAttributes() do
+        if i == "ID" and v == _G.Settings.Mob_Selection_Model then
+        if v2.HealthBar.Enabled then
+            return false -- At least one mob is still alive
+    end
+    end
+    end
+    end
+    return true 
+end
+
+print(AreAllMobsDead())
+
+dwasd = AreAllMobsDead()
