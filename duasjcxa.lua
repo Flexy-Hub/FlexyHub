e16e867a4d8a7a91c4d1360e9cbc78f78245f919
Flexy-Hub <unknownflexyz@gmail.com>
2025-04-01 17:35:34 +0700

    Update duasjcxa.lua

--- duasjcxa.lua
+++ duasjcxa.lua
@@ -1,12 +1,16 @@
 _G.Settings = game:service('HttpService'):JSONDecode(readfile("WinnableHub/AriseCrossover.JSON"))
 
+function yessir()
     for i2,v2 in pairs(workspace.__Main.__Enemies.Client:GetChildren()) do
         for i,v in v2:GetAttributes() do
         if i == "ID" and v == _G.Settings.Mob_Selection_Model then
         if v2.HealthBar.Enabled then
-            callout = false -- At least one mob is still alive
+            return false -- At least one mob is still alive
     end
     end
     end
     end
-    callout = true
+    return true
+end
+
+callout = yessir()
