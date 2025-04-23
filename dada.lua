3a5e098133ce1bcf71c74a75b45c3e9430c77ae3
Flexy-Hub <unknownflexyz@gmail.com>
2025-04-23 18:34:39 +0700

    Update dada.lua

--- dada.lua
+++ dada.lua
@@ -1,5 +1,3 @@
-bossname = nil
-
 for i33,v33 in pairs(workspace.__Main.__Enemies.Server:GetChildren()) do
 for i222,v222 in pairs(v33:GetChildren()) do
 ydasdx = v222
@@ -9,7 +7,7 @@ for model2e1,model2e12 in dasujxc do
 for doubleg,doubleb in dasydcs do
 if model2e1 == "Scale" and model2e12 == 2 then
 if doubleg == "HP" and doubleb > 0 then
-bossname = v222.Name
+v222.Size = Vector3.new(5,5,5)
 end
 end
 end
